c0c59a23d2249c0556ec7473e9ab877635f49135
Stefan-Stev <60571585+Stefan-Stev@users.noreply.github.com>
2020-05-27 19:17:58 +0300

    Update proiect.hs

--- proiect.hs
+++ proiect.hs
@@ -1,16 +1,11 @@
 import Control.Monad.Trans.State
 import qualified Data.Map.Lazy as Map
 import Data.Array
-
-type Text = String
-type Pattern = String
-type ArrText = Array Int Char
-type ArrPat = Array Int Char
 type Bmtable = Map.Map Char Int
 
 --sample
-text_sample = "pdsadsaadsarsedadasdas"
-pattern_sample = "are"
+text_sample = "co"
+pattern_sample = "-odsadsa "
 --
 
 badCharacter :: String -> Bmtable
@@ -22,15 +17,23 @@ badCharacter p = execState (st p indices) Map.empty where
     st (x:xs) (y:ys) = do l <- get
                           put (Map.insert x (plen-y-1) l)   --tot adaug la tabel noi valori imrpeuna cu cheile lor (fiecare char cu shift) 
                           st xs ys
+--comp ((length pattern)-1) txtpat pattern bmt
+match :: Int -> String-> String ->Bmtable -> Int 
+match indice txtpat pattern bmt = case comp indice txtpat pattern bmt of
+                           Just val -> val
+                           Nothing -> (-1)
+                                        
+                                       
+                                       
+                             
 
-match :: String-> String ->Bmtable -> Bool
-match txtpat pattern bmt = comp ((length pattern_sample)-1) 
-  where  --de pus fiecare functie top-level
-    comp i = case caseEqual txtpat pattern i ((length pattern_sample)-1) bmt of
-                Nothing -> True
-                Just val -> if val >= length txtpat
-                                then False
-                                else comp val
+comp  :: Int -> String -> String -> Bmtable -> Maybe Int
+comp i txtpat pattern  bmt= case caseEqual txtpat pattern i ((length pattern)-1) bmt of
+                                  Nothing -> Just (i - length pattern+1)
+                                  Just val -> if val >= length txtpat
+                                               then Nothing
+                                               else comp val txtpat pattern  bmt
+
 caseEqual :: String ->String -> Int ->Int ->Bmtable-> Maybe Int
 caseEqual text pattern indicet (-1) bmt     = Nothing   
 caseEqual text pattern indicet indicep bmt= if ((text !! indicet)==(pattern !! indicep ))
@@ -39,9 +42,25 @@ caseEqual text pattern indicet indicep bmt= if ((text !! indicet)==(pattern !! i
                                                Just v->  Just (indicet+v)
                                                Nothing -> Just (indicet +(length pattern))
                                                                                                                                                               
-algoritm = match text_sample pattern_sample (badCharacter pattern_sample)
+algoritm indiceprimar= match indiceprimar text_sample pattern_sample (badCharacter pattern_sample)
 
-main :: IO()
-main = do
-       let x=badCharacter pattern_sample
+algoritm1::Int -> [Int] -> [Int]
+algoritm1 indiceprimar list= if (indiceprimar+length pattern_sample)>= length text_sample
+                                 then do
+                                 reverse list
+                                 else
+                                 case algoritm indiceprimar of 
+                                 (-1) -> reverse (list)   
+                                 val-> algoritm1 (val + length (pattern_sample)) (val : list)
+                         
+
+
+main Int :: IO()
+main s = do
+       if length pattern_sample > length text_sample
+       then do
+       print ([] :: [Int])
+       else do
+       let x=algoritm1 ((length pattern_sample)-1) []
        print(x)
+      
